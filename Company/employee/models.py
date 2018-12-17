22ae78d1daa135dff5544821b1d504405af88cd8
Islam Mesha <eng.islammesha@gmail.com>
2018-12-17 17:06:08 +0200

    Create our emp model

--- Company/employee/models.py
+++ Company/employee/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django_countries.fields import CountryField
 
-# Create your models here.
+POSITIONS = (
+    ('FR', 'Freshman'),
+    ('SO', 'Sophomore'),
+    ('JR', 'Junior'),
+    ('SR', 'Senior'),
+    ('manager', 'Manager'),
+    ('developer', 'Developer'),
+)
+
+GENDER_CHOICES = (
+    ('M', 'Male'),
+    ('F', 'Female'),
+)
+
+MILITARY_STATUS = (
+    ('ex', 'Exempted'),
+    ('co', 'Completed'),
+    ('re', 'In Resume'),
+)
+
+
+# Multiple Inheritance
+class Employee(User, models.Model):
+    middle_name = models.CharField(max_length=100, null=True, default='')
+    full_name = models.CharField(max_length=200, null=True, default='')
+    national_identifier = models.IntegerField(null=True, default='')
+    job_positions = models.CharField(max_length=100, choices=POSITIONS)
+    age = models.IntegerField(null=True, default='')
+    date_of_birth = models.DateField(name='Date Of Birth', auto_now_add=True)
+    place_of_birth = models.CharField(max_length=100, null=True, default='')
+    country = CountryField()
+    nationality = models.CharField(max_length=100, null=True, default='')
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    military_status = models.CharField(max_length=2, choices=MILITARY_STATUS)
