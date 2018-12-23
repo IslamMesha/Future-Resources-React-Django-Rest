4d7c7e0ff6d393ec61dd48bff8e422649bf4b3b8
Islam Mesha <eng.islammesha@gmail.com>
2018-12-23 10:04:41 +0200

    Draw a table

--- company-fornt_end/src/App.js
+++ company-fornt_end/src/App.js
@@ -1,28 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            employees: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:8000/employees/?page=1', {headers: {'Access-Control-Allow-Origin': '*'}})
+            .then(response => {
+                debugger;
+                this.setState({employees: response.data.results});
+            })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <p> My First React App </p>
+                    <table>
+                        <tr>
+                            <td>ID</td>
+                            <td>First Name</td>
+                            <td>Last Name</td>
+                            <td>Middle Name</td>
+                        </tr>
+                        {this.state.employees.map(emp => (
+                            <tr key={emp.id}>
+                                <td>{emp.id}</td>
+                                <td>{emp.first_name}</td>
+                                <td>{emp.middle_name}</td>
+                                <td>{emp.last_name}</td>
+                            </tr>
+                        ))}
+                    </table>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
