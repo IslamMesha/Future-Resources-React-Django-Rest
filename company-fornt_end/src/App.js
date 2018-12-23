9fd42dde213396d12fffc5ced2a27791bac54223
Islam Mesha <eng.islammesha@gmail.com>
2018-12-23 16:27:01 +0200

    Add some buttons

--- company-fornt_end/src/App.js
+++ company-fornt_end/src/App.js
@@ -3,19 +3,17 @@ import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
-            employees: []
+            employees: [],
+            pageNumber: 1
         }
     }
 
     componentDidMount() {
-        axios.get('http://localhost:8000/employees/?page=1', {headers: {'Access-Control-Allow-Origin': '*'}})
-            .then(response => {
-                debugger;
-                this.setState({employees: response.data.results});
-            })
+        this.getEmployeeList()
     }
 
     render() {
@@ -24,11 +22,13 @@ class App extends Component {
                 <header className="App-header">
                     <p> My First React App </p>
                     <table>
+                        <tbody>
                         <tr>
                             <td>ID</td>
                             <td>First Name</td>
                             <td>Last Name</td>
                             <td>Middle Name</td>
+                            <td>Options</td>
                         </tr>
                         {this.state.employees.map(emp => (
                             <tr key={emp.id}>
@@ -36,12 +36,50 @@ class App extends Component {
                                 <td>{emp.first_name}</td>
                                 <td>{emp.middle_name}</td>
                                 <td>{emp.last_name}</td>
+                                <td>
+                                    <button onClick={this.editEmployeeDatail}>Edit</button>
+                                    <button onClick={this.getEmployeeDatail}>Detail</button>
+                                    <button onClick={() => this.deleteEmployee(emp)}>Delete</button>
+                                </td>
                             </tr>
                         ))}
+                        </tbody>
                     </table>
+                    <div>
+                        <button onClick={this.getNextPage}>Next</button>
+                        <button onClick={this.getPreviousPage}>Previous</button>
+                    </div>
                 </header>
             </div>
         );
+    }
+
+
+    getEmployeeList() {
+        axios.get('http://localhost:8000/employees/?page=' + this.state.pageNumber, {headers: {'Access-Control-Allow-Origin': '*'}})
+            .then(response => {
+                this.setState({employees: response.data.results});
+            });
+    }
+
+    getEmployeeDatail() {
+        console.log("getEmployeeDatail")
+    }
+
+    getNextPage() {
+        console.log("getNextPage")
+    }
+
+    getPreviousPage() {
+        console.log("getPreviousPage")
+    }
+
+    editEmployeeDatail() {
+        console.log("editEmployeeDatail")
+    }
+
+    deleteEmployee(emp) {
+        console.log("deleteEmployee" + Object.keys(emp))
     }
 }
 
